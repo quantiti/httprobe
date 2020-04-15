b8acfd961368a9daf8c9d0b0af711207d7dbdc3f
Quan Truong <quantt86@gmail.com>
2020-04-15 14:53:20 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -216,9 +216,9 @@ func main() {
 
 func isListening(client *http.Client, url, method string) string {
 	//quantt - add to check redirect
-	client.CheckRedirect = func(req1 *http.Request, via []*http.Request) errors {
-        return errors.New("Redirect")
-    }
+	//client.CheckRedirect = func(req1 *http.Request, via []*http.Request) errors {
+    //    return errors.New("Redirect")
+    //}
 	//
 	
 	req, err := http.NewRequest(method, url, nil)
